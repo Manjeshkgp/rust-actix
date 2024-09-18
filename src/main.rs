5aa613a3b09278a41f0540e66f7c4c19e515bfd3
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-09-18 18:31:54 +0530

    JSON Responsing learnt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, web::Path, App, HttpServer, Responder};
+use actix_web::{get, http::StatusCode, web::{Json, Path}, App, HttpServer, Responder};
+use serde::Serialize;
 
 #[get("/home")]
 async fn home() -> impl Responder{
@@ -8,8 +9,21 @@ async fn home() -> impl Responder{
 
 #[get("/hello/{firstname}/{lastname}")]
 async fn hello_user(params:Path<(String,String)>)-> impl Responder{
-    let response:String = format!("Hello {} {}",params.0,params.1);
-    response
+    let response =  User::new(params.0.clone(), params.1.clone());
+    (Json(response),StatusCode::OK)
+}
+
+#[derive(Serialize)]
+
+struct User {
+    first_name:String,
+    last_name:String
+}
+
+impl User {
+    fn new(firstname:String,lastname:String)->Self{
+        Self { first_name: firstname, last_name: lastname }
+    }
 }
 
 #[actix_web::main]
