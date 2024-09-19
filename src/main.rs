1a457c3c94ed761d6633be92f43533dd1b9087e3
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-09-20 02:12:24 +0530

    Code refactoring to modules and tokio runtime added

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,9 @@
-use actix_web::{get, http::StatusCode, web::{Json, Path}, App, HttpServer, Responder};
-use serde::Serialize;
+use actix_web::{App, HttpServer};
 
-#[get("/home")]
-async fn home() -> impl Responder{
-    let response: &str = "Welcome to actix web sever";
-    response
-}
+mod routes;
+use routes::*;
 
-#[get("/hello/{firstname}/{lastname}")]
-async fn hello_user(params:Path<(String,String)>)-> impl Responder{
-    let response =  User::new(params.0.clone(), params.1.clone());
-    (Json(response),StatusCode::OK)
-}
-
-#[derive(Serialize)]
-
-struct User {
-    first_name:String,
-    last_name:String
-}
-
-impl User {
-    fn new(firstname:String,lastname:String)->Self{
-        Self { first_name: firstname, last_name: lastname }
-    }
-}
-
-#[actix_web::main]
+#[tokio::main]
 async fn main()->std::io::Result<()> {
     let server = HttpServer::new(||{
         App::new()
---
+++ src/routes/home.rs
@@ -0,0 +1,9 @@
+use actix_web::{get, Responder};
+use crate::routes::logging;
+
+#[get("/home")]
+async fn home() -> impl Responder{
+    logging("GET: /home");
+    let response: &str = "Welcome to actix web sever";
+    response
+}
