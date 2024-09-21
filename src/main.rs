99ffb8b8bc7b7663e2b404861aa0820fd2537ad9
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-09-21 12:49:31 +0530

    Create and Read part of CRUD is done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, web::Data};
 use dotenv::dotenv;
 
 mod routes;
@@ -6,6 +6,7 @@ use routes::*;
 
 mod database;
 use database::*;
+use sqlx::Executor;
 
 #[tokio::main]
 async fn main()->std::io::Result<()> {
@@ -13,12 +14,23 @@ async fn main()->std::io::Result<()> {
     // running this will allow to load variables from .env file
     let database: sqlx::Pool<sqlx::MySql> = database_connection().await.expect("Failed to connect to the database");
     println!("Database connected");
+    let create_table_sql = r#"
+    CREATE TABLE IF NOT EXISTS todos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
+        description VARCHAR(100) NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    "#;
+    let _ = database.execute(create_table_sql).await;
     let server = HttpServer::new(move||{
         App::new()
-        .app_data(database.clone())
+        .app_data(Data::new(database.clone()))
         .service(home)
         .service(hello_user)
         .service(create_new_user)
+        .service(create_new_todo)
+        .service(get_all_todos)
     }).bind(("127.0.0.1",8080))?.run();
     println!("Server running on PORT 127.0.0.1:8080");
     server.await
